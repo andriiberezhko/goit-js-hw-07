bdcff3564a7d9bc45afea43c2526c7cbdeb3e157
Andrii Berezhko <Berezhko7777@ukr.net>
2022-04-18 23:26:23 +0300

    1
    start 1 task (finish part one)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+
+const makeGalleryElement = ({preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    
+};
+const makeImageGallery = galleryItems.map(makeGalleryElement).join(" ");
+galleryBox.insertAdjacentHTML("afterbegin", makeImageGallery);
+
+galleryBox.addEventListener('click', onOpenGalleryElement);
+
+function onOpenGalleryElement(event) {
+    event.preventDefault();
+
+    // window.addEventListener('keydown', onEscKeyPress)
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const newSrc = event.target.dataset.source;
+    const html = `<img width="1400" height="900" src="${newSrc}">`;
+    
+    const instance = basicLightbox.create(html, newSrc,);
+    instance.show(() => console.log('lightbox now visible'));
+    // instance.close(() => console.log('lightbox not visible anymore'));
+    setTimeout(() => {
+		instance.close(() => console.log('finished close()'))
+	}, 3000)
+     
+}
