ba5963eb548208b57819fc02c7d994419d22df94
Andrii Berezhko <Berezhko7777@ukr.net>
2022-04-19 20:20:01 +0300

    1
    сделал, но доп задание сделано криво

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,19 +25,32 @@ galleryBox.addEventListener('click', onOpenGalleryElement);
 
 function onOpenGalleryElement(event) {
     event.preventDefault();
-
-    // window.addEventListener('keydown', onEscKeyPress)
     if (event.target.nodeName !== 'IMG') {
-        return;
+    return;
     }
     const newSrc = event.target.dataset.source;
     const html = `<img width="1400" height="900" src="${newSrc}">`;
     
-    const instance = basicLightbox.create(html, newSrc,);
-    instance.show(() => console.log('lightbox now visible'));
-    // instance.close(() => console.log('lightbox not visible anymore'));
-    setTimeout(() => {
-		instance.close(() => console.log('finished close()'))
-	}, 3000)
-     
+    const modal = basicLightbox.create(html, newSrc);
+    openModal(modal);
+    
+    closeModalByEsc(modal);
+
+}
+
+function openModal(open) {
+    open.show();
+   
+}
+function closeModalByEsc(close) {
+    if (close.visible()) {
+        window.addEventListener('keydown', pressEscBtn)
+    }
+}
+
+function pressEscBtn(event) {
+    if (event.code === 'Escape') {
+        document.querySelector('.basicLightbox').remove();
+        
+    }
 }
